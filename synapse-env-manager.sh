7e458d8029467b35fdc04a832d2471b61b54071f
Twilight Sparkle <19155609+Twi1ightSparkle@users.noreply.github.com>
2024-01-03 16:03:09 +0000

    Bump copyright year

--- synapse-env-manager.sh
+++ synapse-env-manager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Quickly spin up a Synapse + Postgres in docker for testing.
-# Copyright (C) 2023  Twilight Sparkle
+# Copyright (C) 2024  Twilight Sparkle
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published
