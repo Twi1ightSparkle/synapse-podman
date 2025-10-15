9e2dc9db75d57b3b1ba8817a63c01e3608d8c31c
Twilight Sparkle <noreply@twilightsparkle.dev>
2025-10-15 10:23:50 +0000

    Disable email required for MAS user reg

--- synapse-env-manager.sh
+++ synapse-env-manager.sh
@@ -679,6 +679,7 @@ function generateMasConfig {
         export masManagement="http://$masHost:$listenPort"
         export swaggerCallback="http://$masHost:$listenPort/api/doc/oauth2-callback"
         yq --inplace '
+            .account.password_registration_email_required = false |
             .account.password_registration_enabled = true |
             .clients[0].client_auth_method = "client_secret_basic" |
             .clients[0].client_id = "0000000000000000000SYNAPSE" |
